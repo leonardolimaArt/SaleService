f8609452b06655dcde01f547dcfd2435bb42550b
Leonardo Lima <54562792+leonardolimaArt@users.noreply.github.com>
2025-07-01 16:40:03 -0400

    Feature: MongoDb integration

--- Repository/IProductReposity.cs
+++ Repository/IProductReposity.cs
@@ -4,10 +4,10 @@ namespace SaleService.Repository;
 
 public interface IProductRepository
 {
-    Task<IEnumerable<Product>> GetProducts();
+    Task<IEnumerable<Product>> GetProducts(int page, int pageSize);
     Task<Product> GetProduct(string id);
-    Task<IEnumerable<Product>> GetProductByName(string name);
-    Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+    Task<IEnumerable<Product>> GetProductByName(string name, int page, int pageSize);
+    Task<IEnumerable<Product>> GetProductByCategory(string categoryName, int page, int pageSize);
     Task CreateProduct(Product product);
     Task<bool> UpdateProduct(Product product);
     Task<bool> DeleteProduct(string id);
